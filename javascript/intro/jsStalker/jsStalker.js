9c0c06859c48af979f6437fc32e3f62de86b071b
Erik Pelagio <pelagio.erikj@gmail.com>
2017-07-29 01:28:06 -0500

    Age calculator in days using basic javascript

--- javascript/intro/jsStalker/jsStalker.js
+++ javascript/intro/jsStalker/jsStalker.js
@@ -1,8 +1,9 @@
 //promting the user
 var firstName = prompt("What is your first name?");
 var lastName = prompt("What is your last name?");
-var age = prompt("How old are you?")
+var age = prompt("How old are you?");
+var fullName = firstName + " " + lastName; 
 
-//Console logging the user response 
-console.log("Your full name is " + firstName + lastName);
+//Console logging the user response
+console.log("Your full name is " + fullName);
 console.log("You are " + age + " Years old");
