3473840464598fbe0e93932ad894c9109dee991f
Erik Pelagio <pelagio.erikj@gmail.com>
2017-08-04 00:05:42 -0500

    functions problems update

---
+++ javascript/functions/functionsprob/functionprob.js
@@ -0,0 +1,30 @@
+// // Creating a function isEven
+// function isEven(num){
+//   // Evaluating the number to check if it's even
+//   if(num % 2 === 0){
+//     // if number is even return true ELSE
+//     return true;
+//   }
+//   // ELSE if number is not even return false
+//   else{
+//     return false;
+//   }
+// }
+// Solution #2 to is Even
+
+// Creating a function isEven
+function isEven(num){
+  // Evaluating the number to check if it's even
+  return num % 2 === 0;
+}
+
+// Factorial function
+function factorial(num){
+  // define a result variable
+  var result = 1;
+  // calculate factorial and store value in result
+  for (var i = 2; i < num; i++) {
+  result *= i;
+  }
+  return result;
+}
