c5a15577d7958901ae45983836b771f63b5f1b86
Erik Pelagio <pelagio.erikj@gmail.com>
2017-08-19 23:20:25 -0500

    Added comment to my moviedb.js

--- javascript/objects/movieDB/moviedb.js
+++ javascript/objects/movieDB/moviedb.js
@@ -1,3 +1,4 @@
+// Movies array
 var movies = [
   {
     title: "The Little Rascals",
@@ -6,20 +7,30 @@ var movies = [
   },
   {
     title: "The Sandlot",
-    hasWatched: false,
+    hasWatched: true,
+    rating: 5
+  },
+  {
+    title: "Remember the Titans",
+    hasWatched: true,
     rating: 5
   }
 
 ];
-
+// Using forEach function
 movies.forEach(function(movies){
+  // creating a string "you have"
   var result = "You have ";
+  // if user has watched
   if (movies.hasWatched) {
+    // returning watched
     result += "watched ";
+    // else not seen
   } else {
     result += "Not seen ";
   }
   result += "\"" + movies.title + "\" - ";
   result += movies.rating + " stars";
+  // Console log my result
   console.log(result);
 });
