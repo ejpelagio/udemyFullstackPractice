679237fba54387308f9436ec4fe055a9b8f85789
Erik Pelagio <pelagio.erikj@gmail.com>
2017-08-24 21:32:24 -0500

    reviewing the Dom

---
+++ DOM/4ways/app.js
@@ -0,0 +1,16 @@
+// first way to select the first  p tags
+
+var p = document.querySelector("#p");
+
+
+// Second way to select the first  p tags
+
+var p2 = document.querySelector("#first");
+
+// Third way to select the first  p tags
+
+var p3 = document.getElementById("first");
+
+// fourth way to select the first  p tags
+
+var p4 = document.getElementByClassName("special")[0];
--- DOM/README.md
+++ DOM/README.md
@@ -1,1 +1,11 @@
 # The DOM
+
+## Methods
+
+The document comes with a bunch of methods for selecting elements.  We're going to learn about the following 5:
+
+* document.getElementById()
+* document.getElementsByClassName()
+* document.getElementsByTagName()
+* document.querySelector()
+* document.querySelectorAll()
